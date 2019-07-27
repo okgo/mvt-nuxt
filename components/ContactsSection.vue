41e4e59a3effbad6f34cd56577c2b8b374158381
Yura Rekechuk <okgo0801@gmail.com>
2019-07-27 13:01:09 +0300

    full layout

---
+++ components/ContactsSection.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  section#contacts.c-contacts-section
+    .container
+      .inline-block.half-width
+        .c-contacts-section__message
+          h2.with-lines Talk to us
+          form.c-contacts-section__form(method="POST")
+            .row
+              label Name
+              input(name="name" placeholder="Your Name" required="required" type="text")
+            .row
+              label E-mail
+              input(name="_replyto" placeholder="Your E-mail" required="required" type="email")
+            .row
+              label Message
+              textarea(name="message" placeholder="Message" required="required")
+            .row
+              button.black(type="submit") Send
+
+      .inline-block.half-width.clearfix
+        .c-contacts-section__content.pull-right
+          h4
+            |MVT HOLDING
+            span Cargo transportation company
+          ul.contacts-list
+            li
+              label Phone
+              p
+                a(href="tel:773-691-4361") 773-691-4361
+            li
+              label Fax
+              p
+                a(href="tel:773-691-4361") 773-691-4361
+            li
+              label Adress
+              p 2725 Thatcher Ave unit 307 River Grove, IL 60171
+</template>
+<script>
+  export default {
+
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,41 @@
           p
             b MVT Holding
             |— is a staple in the industry. Making logistics less hectic while achieving consistently reliable delivery of goods. Always on time. Above the competition.
-          a.button(href="#contacts") CONTACT US
-
+          nuxt-link.button(to="/#contacts") CONTACT US
     section.about-section
       .container
-        h4.with-lines About us
+        h2.with-lines About us
         p Our company provides your business with efficient delivery of goods throughout Mid-West and East Coast regions. Rest assured, team of professionals always strive to deliver your freight on time, every time, and in an immaculate condition. Always the best service in the industry with highest customer satisfaction!
+    section.features-section
+      .container
+        .inline-block
+          h3.with-lines Our Features
+          ul.features-list
+            li
+              span.icon.icon--delivery
+              h5 Fast Delivery
+            li
+              span.icon.icon--arrival
+              h5 Timely Arrival
+            li
+              span.icon.icon--gps
+              h5 GPS Tracking
+            li
+              span.icon.icon--repair
+              h5 Repair
+    contacts-section
 </template>
 
 <script>
+  import ContactsSection from "@/components/ContactsSection";
   export default {
     data() {
       return {
 
       }
+    },
+    components: {
+      ContactsSection
     },
     mounted () {
 
